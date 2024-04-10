cc80760076bc077835fe12056255ac58d703519e
bbensky <brianbensky@gmail.com>
2024-04-09 20:11:05 -0600

    fixing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
-BUILD_DIR="/usr/share/nginx/html/assets"
+ASSET_DIR="/usr/share/nginx/html/assets"
 
-get_env_value() {
-    local VAR_NAME="$1"
-    grep "^${VAR_NAME}=" .env.production | cut -d '=' -f 2-
-}
-while IFS= read -r line || [ -n "$line" ]; do
-    if [[ $line != \#* ]]; then
-        VAR_NAME=$(echo "$line" | cut -d '=' -f 1)
-        VAR_VALUE=$(get_env_value "$VAR_NAME")
-        if [ ! -z "${!VAR_NAME}" ]; then
-            VAR_VALUE="${!VAR_NAME}"
-        fi
-        for file in $BUILD_DIR/*.js; do
-            sed -i "s|__${VAR_NAME}__|${VAR_VALUE}|g" "$file"
-        done
-    fi
-done < .env.production
+for file in $ASSET_DIR/*.js; do
+  sed -i "s|__API_ENDPOINT__|${VITE_BASE_URL}|g" "$file"
+done
+
 nginx -g "daemon off;"
