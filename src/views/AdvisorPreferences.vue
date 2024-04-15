f45e9475af4d2b9ec9aa2bd488da08db9d4b0958
john <john@trailsoffroad.com>
2024-04-15 07:28:50 -0600

    advisor preferences

--- src/views/AdvisorPreferences.vue
+++ src/views/AdvisorPreferences.vue
@@ -1,7 +1,166 @@
 <template>
-  <pre>{{ useUserStore().getUser }}</pre>
+  <v-layout class="justify-center pa-4">
+    <v-card class="w-100">
+      <v-toolbar>
+        <v-toolbar-title>Advisor Preferences</v-toolbar-title>
+        <!-- <v-toolbar-items>
+          <v-btn color="primary">Save</v-btn>
+        </v-toolbar-items> -->
+      </v-toolbar>
+
+      <v-card-text>
+        <div class="text-h6 mb-3">Factor Levers</div>
+        <v-checkbox v-model="preferences.momentum">
+          <template v-slot:label> Momentum </template>
+        </v-checkbox>
+
+        <v-checkbox v-model="preferences.quality">
+          <template v-slot:label> Quality </template>
+        </v-checkbox>
+
+        <v-checkbox v-model="preferences.value">
+          <template v-slot:label> Value </template>
+        </v-checkbox>
+
+        <v-checkbox v-model="preferences.volatility">
+          <template v-slot:label> Volatility </template>
+        </v-checkbox>
+
+        <br />
+
+        <v-row>
+          <v-col cols="6">
+            <div class="mb-10">
+              <div class="text-h6">US Large Cap</div>
+              <v-slider
+                v-model="preferences.large_cap"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+
+            <div class="mb-10">
+              <div class="text-h6">International Large Cap</div>
+              <v-slider
+                v-model="preferences.intl_large_cap"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+
+            <div class="mb-10">
+              <div class="text-h6">Small Cap</div>
+              <v-slider
+                v-model="preferences.small_cap"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+
+            <div class="mb-10">
+              <div class="text-h6">Emerging Markets</div>
+              <v-slider
+                v-model="preferences.emerging_markets"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+
+            <div class="mb-10">
+              <div class="text-h6">Fixed Income</div>
+              <v-slider
+                v-model="preferences.fixed_income"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+
+            <div class="mb-10">
+              <div class="text-h6">Commodities</div>
+              <v-slider
+                v-model="preferences.commodities"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+
+            <div class="mb-10">
+              <div class="text-h6">Bitcoin</div>
+              <v-slider
+                v-model="preferences.bitcoin"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+
+            <div class="mb-10">
+              <div class="text-h6">Cash</div>
+              <v-slider
+                v-model="preferences.cash"
+                :ticks="ticks"
+                :min="0"
+                :max="4"
+                :step="1.0"
+                show-ticks="always"
+                color="primary"
+              ></v-slider>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-layout>
 </template>
 
 <script setup>
-import { useUserStore } from '@/store/user';
+import { ref } from 'vue';
+import { inject } from 'vue';
+
+const $axios = inject('$axios');
+
+const preferences = ref({});
+
+const ticks = {
+  0: 'Ignore',
+  1: 'Low',
+  2: 'Medium',
+  3: 'High',
+  4: 'Maximum',
+};
+
+const getPreferences = async () => {
+  const { data } = await $axios.get(`/api/advisors/preferences/`);
+
+  preferences.value = data;
+};
+
+getPreferences();
 </script>
--- src/views/Client.vue
+++ src/views/Client.vue
@@ -1,18 +1,51 @@
 <template>
   <div>
     <div class="d-flex my-6">
-      <div class="text-h4">{{ client.full_name }}</div>
+      <div class="text-h4">{{ client.full_name }} Values Profile</div>
 
       <v-spacer></v-spacer>
 
       <v-btn
+        color="secondary"
+        text="Generate Recommendation"
+        class="ml-2"
+      ></v-btn>
+
+      <v-btn
         color="primary"
         text="Create Survey"
+        class="ml-2"
         @click="router.push(`/clients/${client.uuid}/survey`)"
       ></v-btn>
     </div>
 
-    <div>{{ client }}</div>
+    <v-row>
+      <v-col cols="6">
+        <v-list>
+          <v-list-item
+            title="First Name"
+            :subtitle="client.first_name"
+          ></v-list-item>
+          <v-list-item
+            title="Last Name"
+            :subtitle="client.last_name"
+          ></v-list-item>
+          <v-list-item title="Email" :subtitle="client.email"></v-list-item>
+        </v-list>
+      </v-col>
+
+      <v-col cols="6">
+        <v-list>
+          <v-list-item title="Account Number" :subtitle="''"></v-list-item>
+          <v-list-item
+            title="Account Custodian"
+            :subtitle="client.advisor.full_name"
+          ></v-list-item>
+          <v-list-item title="Account Type" :subtitle="''"></v-list-item>
+          <v-list-item title="Account Status" :subtitle="''"></v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
