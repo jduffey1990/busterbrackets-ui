8d13c3286ffb34019ea577fa85406e8bf94d3bc1
Jordan Duffey <jordan@getpomarium.com>
2024-07-09 10:16:43 -0600

    terms & conditions improvement- v-dialog

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,22 +3,47 @@
     <v-container>
       <v-row align="center" justify="center">
 
-        <v-btn variant="text" color="primary">
-          Terms & Conditions
-          <v-overlay
-              activator="parent"
-              class="ty"
-              location-strategy="connected"
-              location="top center"
-              origin="bottom center"
+        <!--        <v-btn variant="text" color="primary">-->
+        <!--          Terms & Conditions-->
+        <!--          <v-overlay-->
+        <!--              activator="parent"-->
+        <!--              class="ty"-->
+        <!--              location-strategy="connected"-->
+        <!--              location="top center"-->
+        <!--              origin="bottom center"-->
 
-          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </v-overlay>
+        <!--          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor-->
+        <!--            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco-->
+        <!--            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate-->
+        <!--            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,-->
+        <!--            sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+        <!--          </v-overlay>-->
+        <!--        </v-btn>-->
+
+        <v-btn variant="text" color="primary" @click="showTerms = true">
+          Terms & Conditions
         </v-btn>
+
+        <v-dialog v-model="showTerms" max-width="600px">
+          <v-card>
+            <v-card-title>
+              Terms & Conditions
+            </v-card-title>
+            <v-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="showTerms = false">
+                Close
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
         <v-btn
             href="https://www.linkedin.com/company/getpomarium"
             target="_blank"
@@ -41,5 +66,12 @@
 }
 
 </style>
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      showTerms: false,
+    };
+  },
+};
 </script>
