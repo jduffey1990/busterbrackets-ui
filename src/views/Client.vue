97e34c85516b61519d9b75c880bdef66e2c77ca8
Jordan Duffey <jordan@getpomarium.com>
2024-07-09 12:26:01 -0600

    pie chart update

--- src/views/Client.vue
+++ src/views/Client.vue
@@ -588,7 +588,7 @@ watch(currentTab, (e) => {
 
 const brandColors = [
   '#07152A', '#E1EFFF', '#F9BBA9', '#FFE6B6', '#CF6232', '#CDD0D4',
-  '#903F30', '#0E2F5F', '#FCC35B', '#FEFCF7', '#07152A', '#9CA1AA'
+  '#903F30', '#0E2F5F', '#FCC35B', '#FEFCF7', '#9CA1AA'
 ];
 
 const getUniqueRandomColor = (colors, usedColors) => {
